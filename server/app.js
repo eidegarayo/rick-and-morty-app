5ea59e677ce8bf20e7edcbf8242d8443397ec5e7
eidegarayo <eva@iamtech.tech>
2021-12-27 00:42:22 +0100

    Changed Heroky config

--- server/app.js
+++ server/app.js
@@ -15,6 +15,14 @@ const corsOptions = {
   origin: URL_CLIENT,
 };
 
+// Heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -23,13 +31,5 @@ app.use('/testAPI', testAPIRouter);
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/data', dataRoutes);
-
-// Heroku
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 module.exports = app;
