26b7ea85e89df01e0835833633e5c080aa663ccf
Eva M. Ibañez de Garayo <32354632+eidegarayo@users.noreply.github.com>
2022-01-03 13:10:58 +0100

    Delete start-test.cmd

--- server/start-test.cmd
+++
@@ -1,7 +0,0 @@
-SET NODE_ENV=test
-SET PORT=8080
-SET URL_DB=mongodb://127.0.0.1:27017/rick-and-morty
-SET URL_DB_TEST=mongodb://127.0.0.1:27017/rick-and-morty-test
-SET URL_CLIENT=http://localhost:3000
-SET SECRET=d34·koYU^·fnjksliª!34kci
-npm run test:watch
